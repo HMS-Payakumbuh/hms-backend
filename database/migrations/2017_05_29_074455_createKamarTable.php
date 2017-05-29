b9c39fe47a2693ac0002e2b0d22c8b13354f42b3
alex <alexander.sukono@gmail.com>
2017-05-29 16:17:35 +0700

    Add kamar, tempat tidur, and pemakaian tempat tidur migration

---
+++ database/migrations/2017_05_29_074455_createKamarTable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKamarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kamar', function (Blueprint $table) {
+            $table->integer('no_kamar')->primary();
+            $table->string('jenis_ruangan');
+            $table->integer('kapasitas_pasien');
+            $table->integer('harga_per_hari');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kamar');
+    }
+}
---
+++ database/migrations/2017_05_29_074517_createTempatTidurTable.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTempatTidurTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tempat_tidur', function (Blueprint $table) {
+            $table->integer('no_kamar');
+            $table->integer('no_tempat_tidur');
+            $table->string('status',1);
+            $table->timestamps();
+
+            $table->primary(['no_kamar', 'no_tempat_tidur']);
+            $table->foreign('no_kamar')
+                    ->references('no_kamar')
+                    ->on('kamar')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tempat_tidur');
+    }
+}
