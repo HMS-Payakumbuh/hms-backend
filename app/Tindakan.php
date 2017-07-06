2a6468d97d74dd26a0921295e145a2ec52132556
Calvin <calvin.aditya95@gmail.com>
2017-07-06 14:35:58 +0700

    Add relationship to daftar tindakan

--- app/Tindakan.php
+++ app/Tindakan.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Tindakan extends Model
 {
   protected $table = 'tindakan';
+
+  public function daftarTindakan() {
+    return $this->belongsTo('App\DaftarTindakan');
+  }
 }
