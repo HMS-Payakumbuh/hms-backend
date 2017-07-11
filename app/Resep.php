fd8d9d08f05e9f8be970c3ecdd77022a86b0fdcb
Hans Christian Gunawan <hcguyz@gmail.com>
2017-07-11 11:10:48 +0700

    Revise Resep

--- app/Resep.php
+++ app/Resep.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Resep extends Model
 {
     protected $table = 'resep';
-    protected $primaryKey = 'no_resep';
 
     /**
     *	Get the ResepItem of the Resep
@@ -15,13 +14,5 @@ class Resep extends Model
     public function resepItem()
     {
     	return $this->hasMany('App\ResepItem');
-    }
-
-    /**
-    *	Get the RacikanItem of the Resep
-    */
-    public function racikanItem()
-    {
-    	return $this->hasManyThrough('App\ResepItem', 'App\RacikanItem');
     }
 }
--- database/migrations/2017_06_30_080003_create_racikan_item_table.php
+++ database/migrations/2017_06_30_080003_create_racikan_item_table.php
@@ -16,7 +16,7 @@ class CreateRacikanItemTable extends Migration
         Schema::create('racikan_item', function (Blueprint $table) {
             $table->increments('id');
                     
-            $table->integer('id_item')->unsigned();            
+            $table->integer('resep_item_id')->unsigned();            
             $table->integer('id_jenis_obat')->unsigned();
 
             $table->integer('jumlah');
@@ -24,7 +24,7 @@ class CreateRacikanItemTable extends Migration
             $table->timestamps();
 
             $table
-              ->foreign('id_item')
+              ->foreign('resep_item_id')
               ->references('id')
               ->on('resep_item')
               ->onDelete('restrict');
