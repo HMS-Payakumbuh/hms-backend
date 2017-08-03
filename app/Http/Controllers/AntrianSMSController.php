90ded38297b35011d987b396bf8188798c53d3af
Jonathan Benedict <Jonathan Benedict>
2017-08-03 14:45:25 +0700

    Tweaked all type of antrian

--- app/Http/Controllers/AntrianSMSController.php
+++ app/Http/Controllers/AntrianSMSController.php
@@ -51,21 +51,32 @@ class AntrianSMSController extends Controller
         try {
             $pieces = explode("_", $message);
 
+            $all_antrian = AntrianFrontOffice::all();
+            if (!empty($all_antrian[0])) {
+                if ($all_antrian[0]->waktu_perubahan_antrian < Carbon::today()->toDateTimeString()) {
+                    AntrianFrontOffice::truncate();
+                }
+            } 
+
             $antrian_front_office = new AntrianFrontOffice;
             $antrian_front_office->jenis = 0;
-            $antrian_front_office->kesempatan = 3;
+            $antrian_front_office->kesempatan = 5;
             $antrian_front_office->via_sms = true;
 
             $pasien = Pasien::where('kode_pasien', '=', $pieces[0])->first();
             if ($pasien) 
                 $antrian_front_office->nama_pasien = $pasien->nama_pasien;    
-            else
-                $antrian_front_office->nama_pasien = $pieces[0];
+            else {
+              $text = '[PAYAKUMBUH] Pendaftaran gagal. Kode pasien yang dimasukkan tidak terdaftar.';
+              Log::info('Mengirim SMS ke nomor '.$sender_phone);
+              self::sendMessage($text, $sender_phone);
+              return response($text, 500);
+            }
 
             if (substr($pieces[1], 0, 4) === 'Poli')
                 $antrian_front_office->nama_layanan_poli = $pieces[1];
             else
-                $antrian_front_office->nama_layanan_lab = $pieces[1];
+                $antrian_front_office->nama_layanan_lab = $pieces[1]; 
 
             if ($antrian_front_office->nama_layanan_poli) {
                 $layanan = Poliklinik::where('nama', '=', $antrian_front_office->nama_layanan_poli)->first();
@@ -101,7 +112,10 @@ class AntrianSMSController extends Controller
                 $panjang_antrian = count(AntrianFrontOffice::where('kategori_antrian', '=', $layanan->kategori_antrian)->get());
                 $minutes = $panjang_antrian * 3;
                 $now = Carbon::now();
-                $text = '[PAYAKUMBUH] Pendaftaran berhasil. Anda mendapat nomor antrian '.$antrian_front_office->kategori_antrian.$antrian_front_office->no_antrian.'. Datanglah sebelum Pukul '.$now->copy()->addMinutes($minutes)->toTimeString().'.';
+                $text = '[PAYAKUMBUH] Pendaftaran berhasil. Anda mendapat nomor antrian '.$antrian_front_office->kategori_antrian.$antrian_front_office->no_antrian.'. Datanglah antara Pukul '.$now->copy()->addMinutes($minutes - 15)->toTimeString().' - '.$now->copy()->addMinutes($minutes)->toTimeString().'.';
+                $antrian_front_office->waktu_perjanjian = $now->copy()->addMinutes($minutes)->toTimeString();
+                $antrian_front_office->no_sms = $sender_phone;
+                $antrian_front_office->save();
             }
             Log::info('Mengirim SMS ke nomor '.$sender_phone);
             self::sendMessage($text, $sender_phone);
